813bf565a519a088a70c28ded3d0b74da10ba620
mcferrenm <mcferrenmax@gmail.com>
2019-02-21 16:02:15 -0500

    Add knex config and data access helper boilerplate

---
+++ data/helpers/recipesDb.js
@@ -0,0 +1,35 @@
+const db = require('../dbConfig');
+
+// getDishes(): should return a list of all dishes in the database.
+// addDish(dish): should add the dish to the database and return the id of the new dish.
+// getDish(id): should return the dish with the provided id and include a list of the related recipes.
+// getRecipes(): should return a list of all recipes in the database including the dish they belong to.
+// addRecipe(recipe): should add a recipe to the database and return the id of the new recipe.
+
+module.exports = {
+  getDishes,
+  addDish,
+  getDish,
+  getRecipes,
+  addRecipe
+};
+
+function getDishes() {
+
+}
+
+function getDish(id) {
+
+}
+
+function addDish(dish) {
+
+}
+
+function getRecipes(id) {
+
+}
+
+function addRecipe(id) {
+
+}
